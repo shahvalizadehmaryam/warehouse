23ec818791dd3919d653c668e6b7be32ac9cce8f
Maryam Shahvalizadeh <Shahvalizadehmaryam@gmail.com.>
2024-10-24 10:55:03 +0330

    productstable header component completed.

---
+++ src/components/modules/ProductHeader.jsx
@@ -0,0 +1,15 @@
+import styles from "./ProductHeader.module.css";
+// this part contains header part of table
+const ProductHeader = () => {
+  return (
+    <div className={styles.header}>
+      <div className={styles.manageProduct}>
+        <img src="setting.svg" alt="manageProductIcon" />
+        <span>مدیریت کالا</span>
+      </div>
+      <button>افزودن محصول</button>
+    </div>
+  );
+};
+
+export default ProductHeader;
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,10 +1,12 @@
 
 import Products from "components/modules/Products";
+import ProductHeader from "components/modules/ProductHeader";
 
 const ProductsPage = () => {
 
   return (
    <div>
+    <ProductHeader />
    <Products />
    </div>
   );
